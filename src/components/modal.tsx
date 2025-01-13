51a5e2f152b58cc6d378a331c526624da11bb39a
bodik1987 <bsshul@gmail.com>
2025-01-13 10:03:51 +0100

    Gradient

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {
   isOpen: boolean;
@@ -24,18 +25,34 @@ export default function Modal({
     };
   }, [isOpen]);
 
-  if (!isOpen) return null;
   return (
-    <div
-      className="fixed inset-0 bg-black/50 backdrop-blur-md flex justify-center items-end z-50"
-      onClick={onClose}
-    >
-      <div
-        className={`${modalStyles} max-w-md w-full bg-white dark:bg-app-dark-3 rounded-t-xl relative`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {content}
-      </div>
-    </div>
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.1 },
+          }}
+          exit={{ opacity: 0 }}
+          className="fixed inset-0 bg-black/50 backdrop-blur-md flex justify-center items-end z-50"
+          onClick={onClose}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.1, type: "anticipate" },
+            }}
+            exit={{ opacity: 0, y: "100%" }}
+            className={`${modalStyles} max-w-md w-full bg-white dark:bg-app-dark-3 rounded-t-xl relative`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {content}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
